2708abc36287531640409018582cb217cbd6e457
tim.kaine <tim.kaine@bliss-systems.co.uk>
2015-06-24 15:44:48 +0100

    Adding a basic folder structure and a default mvc site

---
+++ Warhammer/Warhammer.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Warhammer.Mvc.MvcApplication" Language="C#" %>
---
+++ Warhammer/Warhammer.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Warhammer.Mvc.Startup))]
+namespace Warhammer.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
