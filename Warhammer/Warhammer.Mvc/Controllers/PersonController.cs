adecfa8f0b2db0e1058e23cbb69f031b450cb166
Kaine <github@timkaine.co.uk>
2015-06-26 20:30:06 +0100

    Some really basic page editing on the index as a proof of concept (not linked up to anything yet)

---
+++ Warhammer/Warhammer.Mvc/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace Warhammer.Mvc.Controllers
+{
+    public class PersonController : Controller
+    {
+        [HttpPost, ValidateInput(false)]
+        public ActionResult Edit(int personId, string fullName, string shortName, string description, string saveAction)
+        {
+            if (saveAction == "Save")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");              
+            }
+        }
+    }
+}
