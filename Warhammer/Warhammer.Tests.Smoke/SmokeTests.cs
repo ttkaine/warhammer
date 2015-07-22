62b12131801770a100025f60345f2112c586a153
Kaine <github@timkaine.co.uk>
2015-07-22 22:17:43 +0100

    A few more smoke tests for hitting pages

--- Warhammer/Warhammer.Tests.Smoke/SmokeTests.cs
+++ Warhammer/Warhammer.Tests.Smoke/SmokeTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -57,5 +59,52 @@ namespace Warhammer.Tests.Smoke
             _driver.Navigate().GoToUrl(_settings.BaseUrl + "/Home/Graveyard");
             Assert.IsTrue(_driver.PageSource.Contains("The Graveyard"));
         }
+
+        [Test]
+        public void HitCharacterLeague()
+        {
+            _driver.Navigate().GoToUrl(_settings.BaseUrl + "/Home/CharacterLeague");
+            Assert.IsTrue(_driver.PageSource.Contains("League"));
+        }
+
+        [Test]
+        public void HitAFewPageTypePages()
+        {
+            _driver.Navigate().GoToUrl(_settings.BaseUrl);
+            _wait.Until(ExpectedConditions.ElementExists(By.Id("recentChangesList")));
+            IWebElement list = _driver.FindElement(By.Id("recentChangesList"));
+
+            List<string> linkTexts = list.FindElements(By.TagName("a")).Select(l => l.Text).ToList();
+
+            foreach (string linkText in linkTexts)
+            {
+                _driver.FindElement(By.LinkText(linkText)).Click();
+                _wait.Until(ExpectedConditions.ElementExists(By.Id("fullName")));
+                IWebElement titleElement = _driver.FindElement(By.Id("fullName"));
+                Assert.IsTrue(linkText.Contains(titleElement.Text), "Title should be the front of link text");
+                _driver.Navigate().GoToUrl(_settings.BaseUrl);
+                _wait.Until(ExpectedConditions.ElementExists(By.Id("recentChangesList")));              
+            }
+        }
+
+        [Test]
+        public void HitAllTheCharacters()
+        {
+            _driver.Navigate().GoToUrl(_settings.BaseUrl + "/Home/CharacterLeague");
+            _wait.Until(ExpectedConditions.ElementExists(By.Id("characterLeagueList")));
+            IWebElement list = _driver.FindElement(By.Id("characterLeagueList"));
+
+            List<string> linkIds = list.FindElements(By.TagName("a")).Select(l => l.GetAttribute("Id")).ToList();
+
+            foreach (string linkId in linkIds)
+            {
+                _driver.FindElement(By.Id(linkId)).Click();
+                _wait.Until(ExpectedConditions.ElementExists(By.Id("fullName")));
+                Assert.IsNotNull(ExpectedConditions.ElementExists(By.Id("fullName")));
+                _driver.Navigate().GoToUrl(_settings.BaseUrl + "/Home/CharacterLeague");
+                _wait.Until(ExpectedConditions.ElementExists(By.Id("characterLeagueList")));
+            }
+        }
+
     }
 }
